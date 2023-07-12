c12a24f567179506b85f8dbb9225be0f53f0a4cf
Walter_Mena <Walthermena0799@gmail.com>
2023-07-12 15:27:08 -0600

    Agregar Capa EN y clases EN

---
+++ Esfe.Store.EN/DetallePedidoEN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfe.Store.EN
+{
+    public class DetallePedidoEN
+    {
+        public int Id { get; set; }
+        public int IdPedido { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
---
+++ Esfe.Store.EN/PedidoEN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfe.Store.EN
+{
+    public class PedidoEN
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime FechaPedido { get; set; }
+        public byte Estado { get; set; }
+        public decimal Total { get; set; }
+    }
+}
