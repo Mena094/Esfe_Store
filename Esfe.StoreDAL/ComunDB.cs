3a992035d98f40fa9bb64a7bcd159ce64e371424
Walter_Mena <Walthermena0799@gmail.com>
2023-07-12 15:51:10 -0600

    Programar ComunDB

--- Esfe.StoreDAL/ComunDB.cs
+++ Esfe.StoreDAL/ComunDB.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Esfe.StoreDAL
 {
-    internal class ComunDB
+    public class ComunDB
     {
+        //---Se coloca el link de conexion en una constante
+        const string StrConexion = @"";
+
+        //----Establecemos la conexion con SQL
+        private static SqlConnection ObtenerConexion()
+        {
+            SqlConnection connection = new SqlConnection(StrConexion);
+            connection.Open();
+            return connection;
+        }
+        public static SqlCommand ObtenerComando()
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = ObtenerConexion();
+            return command;
+        }
+        public static int EjecutarComando(SqlCommand pComando)
+        {
+            int resultado = pComando.ExecuteNonQuery();
+            pComando.Connection.Close();
+            return resultado;
+        }
+        public static SqlDataReader EjecutarComandoReader(SqlCommand pComando)
+        {
+            SqlDataReader reader = pComando.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
     }
 }
